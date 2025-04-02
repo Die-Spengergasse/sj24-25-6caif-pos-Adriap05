aeb20cc45fe4ed620682ba263af18da5551397f2
Adrian Plank <pla21946@spengergasse.at>
2025-04-02 19:57:05 +0200

    V1

---
+++ Asp_Wiederholung_6CAIF/src/SPG_Fachtheorie.Aufgabe1/Services/PaymentService.cs
@@ -0,0 +1,28 @@
+﻿using SPG_Fachtheorie.Aufgabe1.Commands;
+using SPG_Fachtheorie.Aufgabe1.Infrastructure;
+using SPG_Fachtheorie.Aufgabe1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPG_Fachtheorie.Aufgabe1.Services
+{
+    public class PaymentService
+    {
+        private readonly AppointmentContext _db;
+
+        public PaymentService(AppointmentContext db)
+        {
+            _db = db;
+        }
+
+        public IQueryable<Employee> Payments => _db.Payments.AsQueryable();
+
+        public Payment CreatePayment(NewPaymentCommand cmd)
+        {
+
+        }
+    }
+}
