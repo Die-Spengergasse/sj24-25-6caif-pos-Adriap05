0aa58a4376fca9c48623b4399370da2cc84ac195
Adrian Plank <pla21946@spengergasse.at>
2025-05-09 18:44:16 +0200

    V1

---
+++ Asp_Wiederholung/test/SPG_Fachtheorie.Aufgabe3.Test/PaymentsControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using Spg.Fachtheorie.Aufgabe3.API.Test;
+using SPG_Fachtheorie.Aufgabe1.Model;
+using SPG_Fachtheorie.Aufgabe3.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SPG_Fachtheorie.Aufgabe3.Test
+{
+    public class PaymentsControllerTests
+    {
+        public async Task GetAllPaymentsTests(int CashDesk, HttpStatusCode expectedStatusCode)
+        {
+            // ARRANGE
+            var factory = new TestWebApplicationFactory();
+            factory.InitializeDatabase(db =>
+            {
+                var cashDesk = new CashDesk(1); // richtig ???
+                var cashier = new Cashier(
+                    2, "FN", "LN", new DateOnly(2004, 2, 1),
+                    3000M, null, "Feinkost");
+                var payment = new Payment(cashDesk, new DateTime(2025, 05, 05), 
+                    cashier, PaymentType.Cash);
+                db.AddRange(payment);
+                db.SaveChanges();
+            });
+
+            // ACT
+            var (statusCode, content) = await factory.GetHttpContent<List<PaymentDto>>($"/api/payments?cashDesk=1");
+
+            // ASSERT
+            Assert.True(statusCode == System.Net.HttpStatusCode.OK);
+            Assert.NotNull(content);
+            Assert.True(content.First().R);
+
+        }
+    }
+}
