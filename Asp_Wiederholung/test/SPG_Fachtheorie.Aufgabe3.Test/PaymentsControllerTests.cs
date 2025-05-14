decef89f4ff16b3f5dc562b311b15736030c17ba
Adrian Plank <pla21946@spengergasse.at>
2025-05-14 20:13:13 +0200

    V2

--- Asp_Wiederholung/test/SPG_Fachtheorie.Aufgabe3.Test/PaymentsControllerTests.cs
+++ Asp_Wiederholung/test/SPG_Fachtheorie.Aufgabe3.Test/PaymentsControllerTests.cs
@@ -1,5 +1,8 @@
 ﻿using Spg.Fachtheorie.Aufgabe3.API.Test;
+using SPG_Fachtheorie.Aufgabe1.Commands;
 using SPG_Fachtheorie.Aufgabe1.Model;
+using SPG_Fachtheorie.Aufgabe1.Services;
+using SPG_Fachtheorie.Aufgabe3.Controllers;
 using SPG_Fachtheorie.Aufgabe3.Dtos;
 using System;
 using System.Collections.Generic;
@@ -13,29 +16,35 @@ namespace SPG_Fachtheorie.Aufgabe3.Test
 {
     public class PaymentsControllerTests
     {
-        public async Task GetAllPaymentsTests(int CashDesk, HttpStatusCode expectedStatusCode)
+        [Theory]
+        [InlineData(1, HttpStatusCode.OK)] // GET /api/payments?cashDesk = 1
+        [InlineData(2, HttpStatusCode.NotFound)] 
+
+        public async Task GetAllPaymentsTests(
+            int cashDeskNumber, HttpStatusCode expectedStatusCode)
         {
             // ARRANGE
             var factory = new TestWebApplicationFactory();
             factory.InitializeDatabase(db =>
             {
-                var cashDesk = new CashDesk(1); // richtig ???
+            });
+            var cashDesk = new CashDesk(1);
                 var cashier = new Cashier(
                     2, "FN", "LN", new DateOnly(2004, 2, 1),
                     3000M, null, "Feinkost");
                 var payment = new Payment(cashDesk, new DateTime(2025, 05, 05), 
                     cashier, PaymentType.Cash);
-                db.AddRange(payment);
-                db.SaveChanges();
-            });
+                //db.AddRange(payment);
+                //db.SaveChanges();
+           
 
             // ACT
-            var (statusCode, content) = await factory.GetHttpContent<List<PaymentDto>>($"/api/payments?cashDesk=1");
+            var (statusCode, content) = await factory.GetHttpContent<List<PaymentDto>>($"/api/payments/{cashDeskNumber}");
 
             // ASSERT
-            Assert.True(statusCode == System.Net.HttpStatusCode.OK);
+            Assert.True(statusCode == expectedStatusCode);
             Assert.NotNull(content);
-            Assert.True(content.First().R);
+            Assert.True(content.First().CashDeskNumber == cashDeskNumber);
 
         }
     }
